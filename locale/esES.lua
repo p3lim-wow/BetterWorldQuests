f239b7e5eb5a971756f5b8c7f15b721aee364b9c
Adrian L Lange <git@p3lim.net>
2023-11-21 19:31:56 +0100

    Add localization files

---
+++ locale/esES.lua
@@ -0,0 +1,9 @@
+local L = select(2, ...).L('esES')
+
+-- config
+-- L['Map pin scale'] = '' -- MISSING!
+-- L['The scale of world quest pins on the current map'] = '' -- MISSING!
+-- L['Overview pin scale'] = '' -- MISSING!
+-- L['The scale of world quest pins on a parent/continent map'] = '' -- MISSING!
+-- L['Pin size zoom factor'] = '' -- MISSING!
+-- L['How much extra scale to apply when map is zoomed'] = '' -- MISSING!
