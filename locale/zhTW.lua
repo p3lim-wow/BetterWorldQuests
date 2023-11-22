b3d79e052a0da6555b2e0840fc2eb1d9771240e0
Adrian L Lange <git@p3lim.net>
2023-11-22 17:57:22 +0100

    Add zhTW translations (@BlueNightSky)

--- locale/zhTW.lua
+++ locale/zhTW.lua
@@ -1,9 +1,9 @@
--- local L = select(2, ...).L('zhTW')
+local L = select(2, ...).L('zhTW')
 
 -- config
--- L['Map pin scale'] = '' -- MISSING!
--- L['The scale of world quest pins on the current map'] = '' -- MISSING!
--- L['Overview pin scale'] = '' -- MISSING!
--- L['The scale of world quest pins on a parent/continent map'] = '' -- MISSING!
--- L['Pin size zoom factor'] = '' -- MISSING!
--- L['How much extra scale to apply when map is zoomed'] = '' -- MISSING!
+L['Map pin scale'] = '地圖標記點大小'
+L['The scale of world quest pins on the current map'] = '在當前地圖的世界任務標記點大小'
+L['Overview pin scale'] = '總覽標記點大小'
+L['The scale of world quest pins on a parent/continent map'] = '在大陸地圖的世界任務標記點大小'
+L['Pin size zoom factor'] = '標記點大小縮放係數'
+L['How much extra scale to apply when map is zoomed'] = '縮放地圖時要應用多少額外的縮放'
