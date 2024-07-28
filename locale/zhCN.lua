43241103226062f9bf02ac7219e90d2c9aabef1d
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-07-28 09:50:52 +0800

    Update zhCN.lua

--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -1,10 +1,10 @@
--- local L = select(2, ...).L('zhCN')
+local L = select(2, ...).L('zhCN')
 
 -- config
--- L['Map pin scale'] = '' -- MISSING!
--- L['The scale of world quest pins on the current map'] = '' -- MISSING!
--- L['Overview pin scale'] = '' -- MISSING!
--- L['The scale of world quest pins on a parent/continent map'] = '' -- MISSING!
--- L['Pin size zoom factor'] = '' -- MISSING!
--- L['How much extra scale to apply when map is zoomed'] = '' -- MISSING!
--- L['Pin border color'] = '' -- MISSING!
+L['Map pin scale'] = '地图图标大小'
+L['The scale of world quest pins on the current map'] = '当前地图的任务图标大小'
+L['Overview pin scale'] = '浏览图标大小'
+L['The scale of world quest pins on a parent/continent map'] = '世界地图的任务图标大小'
+L['Pin size zoom factor'] = '图标大小比例'
+L['How much extra scale to apply when map is zoomed'] = '地图缩放时图标的比例'
+L['Pin border color'] = '图标边框颜色'
