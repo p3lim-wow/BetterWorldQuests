0ad0be744b4979d2f462b03a26c273b2a3515ad4
Adrian L Lange <git@p3lim.net>
2024-07-28 18:51:04 +0200

    Add new strings for new options

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -7,3 +7,5 @@
 -- L['The scale of world quest pins on a parent/continent map'] = '' -- MISSING!
 -- L['Pin size zoom factor'] = '' -- MISSING!
 -- L['How much extra scale to apply when map is zoomed'] = '' -- MISSING!
+-- L['Hold key to hide'] = '' -- MISSING!
+-- L['Hold this key to temporarily hide all world quests'] = '' -- MISSING!
--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -7,3 +7,5 @@ L['Overview pin scale'] = '浏览图标大小'
 L['The scale of world quest pins on a parent/continent map'] = '世界地图的任务图标大小'
 L['Pin size zoom factor'] = '图标大小比例'
 L['How much extra scale to apply when map is zoomed'] = '地图缩放时图标的比例'
+-- L['Hold key to hide'] = '' -- MISSING!
+-- L['Hold this key to temporarily hide all world quests'] = '' -- MISSING!
