4d7ddde34f82c7d611538d8e59976f87a0a66e86
Adrian L Lange <git@p3lim.net>
2024-08-20 00:33:53 +0200

    Add deDE translations
    Fixes #50
    
    Thanks @DlargeX

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -1,11 +1,11 @@
--- local L = select(2, ...).L('deDE')
+local L = select(2, ...).L('deDE')
 
 -- config
--- L['Map pin scale'] = '' -- MISSING!
--- L['The scale of world quest pins on the current map'] = '' -- MISSING!
--- L['Overview pin scale'] = '' -- MISSING!
--- L['The scale of world quest pins on a parent/continent map'] = '' -- MISSING!
--- L['Pin size zoom factor'] = '' -- MISSING!
--- L['How much extra scale to apply when map is zoomed'] = '' -- MISSING!
--- L['Hold key to hide'] = '' -- MISSING!
--- L['Hold this key to temporarily hide all world quests'] = '' -- MISSING!
+L['Map pin scale'] = 'Skalierung der Kartennadel'
+L['The scale of world quest pins on the current map'] = 'Skalierung der Weltquest-Nadeln auf der aktuellen Karte'
+L['Overview pin scale'] = 'Übersicht Nadelskala'
+L['The scale of world quest pins on a parent/continent map'] = 'Skalierung der Weltquest-Nadeln auf einem Welt-/Kontinentkarte'
+L['Pin size zoom factor'] = 'Zoomfaktor der Nadelgröße'
+L['How much extra scale to apply when map is zoomed'] = 'Wie viel zusätzlicher Maßstab angewendet werden soll, wenn die Karte gezoomt wird?'
+L['Hold key to hide'] = 'Zum Ausblenden die Taste gedrückt halten'
+L['Hold this key to temporarily hide all world quests'] = 'Halte diese Taste gedrückt, um alle Weltquests vorübergehend auszublenden'
