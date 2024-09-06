c33a0d79de628a577d03e63c2c361190811ab98a
Adrian L Lange <git@p3lim.net>
2024-09-06 07:59:49 +0200

    Update zhCN translations
    Fixes #60

--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -1,13 +1,13 @@
 local L = select(2, ...).L('zhCN')
 
 -- config
-L['Map pin scale'] = '地图图标大小'
-L['The scale of world quest pins on the current map'] = '当前地图的任务图标大小'
-L['Overview pin scale'] = '浏览图标大小'
-L['The scale of world quest pins on a parent/continent map'] = '世界地图的任务图标大小'
-L['Pin size zoom factor'] = '图标大小比例'
-L['How much extra scale to apply when map is zoomed'] = '地图缩放时图标的比例'
--- L['Show events on continent'] = '' -- MISSING TRANSLATION
-L['Show on Azeroth'] = '在艾泽拉斯世界地图上显示'
-L['Hold key to hide'] = '隐藏键'
-L['Hold this key to temporarily hide all world quests'] = '按住此键暂时隐藏所有世界任务'
+L['Map pin scale'] = '地图标记缩放'
+L['The scale of world quest pins on the current map'] = '当前地图上世界任务标记的缩放比例'
+L['Overview pin scale'] = '概览地图标记缩放'
+L['The scale of world quest pins on a parent/continent map'] = '在父地图/大陆地图上的世界任务标记的缩放比例'
+L['Pin size zoom factor'] = '标记大小缩放因子'
+L['How much extra scale to apply when map is zoomed'] = '当地图放大时应用的额外缩放比例'
+L['Show events on continent'] = '在大陆地图上显示事件'
+L['Show on Azeroth'] = '在艾泽拉斯上显示'
+L['Hold key to hide'] = '按住键以隐藏'
+L['Hold this key to temporarily hide all world quests'] = '按住此键临时隐藏所有世界任务'
