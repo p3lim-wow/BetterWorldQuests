d700bfc2482ddcbd1d633c9fedb3e2c38255e91a
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-12-06 18:30:19 +0800

    Update zhCN.lua

--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -1,12 +1,12 @@
 local L = select(2, ...).L('zhCN')
 
 -- config
-L['Map pin scale'] = '地图标记缩放'
+L['Map pin scale'] = '地图任务标记缩放'
 L['The scale of world quest pins on the current map'] = '当前地图上世界任务标记的缩放比例'
-L['Overview pin scale'] = '概览地图标记缩放'
-L['The scale of world quest pins on a parent/continent map'] = '在父地图/大陆地图上的世界任务标记的缩放比例'
-L['Pin size zoom factor'] = '标记大小缩放因子'
-L['How much extra scale to apply when map is zoomed'] = '当地图放大时应用的额外缩放比例'
+L['Overview pin scale'] = '概览地图任务标记缩放'
+L['The scale of world quest pins on a parent/continent map'] = '在主地图/大陆地图上的世界任务标记的缩放比例'
+L['Pin size zoom factor'] = '任务标记缩放比例'
+L['How much extra scale to apply when map is zoomed'] = '当放大地图时任务标记的额外缩放比例'
 L['Show events on continent'] = '在大陆地图上显示事件'
 L['Show on Azeroth'] = '在艾泽拉斯上显示'
 L['Hold key to hide'] = '按住此键隐藏'
