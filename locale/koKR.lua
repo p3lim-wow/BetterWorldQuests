bacf836c2869067afe3d99489813e66c057f94f1
Adrian L Lange <git@p3lim.net>
2024-12-19 03:47:10 +0100

    Remove untranslated strings

--- locale/koKR.lua
+++ locale/koKR.lua
@@ -1,13 +1,1 @@
--- local L = select(2, ...).L('koKR')
-
--- config
--- L['Map pin scale'] = '' -- MISSING!
--- L['The scale of world quest pins on the current map'] = '' -- MISSING!
--- L['Overview pin scale'] = '' -- MISSING!
--- L['The scale of world quest pins on a parent/continent map'] = '' -- MISSING!
--- L['Pin size zoom factor'] = '' -- MISSING!
--- L['How much extra scale to apply when map is zoomed'] = '' -- MISSING!
--- L['Show events on continent'] = '' -- MISSING TRANSLATION
--- L['Show on Azeroth'] = '' -- MISSING TRANSLATION
--- L['Hold key to hide'] = '' -- MISSING!
--- L['Hold this key to temporarily hide all world quests'] = '' -- MISSING!
+local L = select(2, ...).L('koKR') -- luacheck: ignore
--- locale/zhTW.lua
+++ locale/zhTW.lua
@@ -7,7 +7,3 @@ L['Overview pin scale'] = '總覽標記點大小'
 L['The scale of world quest pins on a parent/continent map'] = '在大陸地圖的世界任務標記點大小'
 L['Pin size zoom factor'] = '標記點大小縮放係數'
 L['How much extra scale to apply when map is zoomed'] = '縮放地圖時要應用多少額外的縮放'
--- L['Show events on continent'] = '' -- MISSING TRANSLATION
--- L['Show on Azeroth'] = '' -- MISSING TRANSLATION
--- L['Hold key to hide'] = '' -- MISSING!
--- L['Hold this key to temporarily hide all world quests'] = '' -- MISSING!
